be7de1e31e649939860745c52138a4e4e7b43ccd
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-04-15 03:42:35 +0530

    3 part almost completed

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../../containers/LandingPage/HeroSection'
 import About from '../../containers/LandingPage/About'
+import Listners from '../../containers/LandingPage/Listners'
 
 const LandingPage = () => {
   return (
     <>
     <HeroSection/>
     <About/>
+    <Listners/>
     </>
   )
 }
